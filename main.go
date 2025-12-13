8b78485d3a1e5212b6878e21c14c9d0100e4fd6a
Stian M <otard.code@proton.me>
2025-12-13 12:08:45 +0100

    feat(main): Initialize journal watcher at startup
    - Require -j flag for journal directory
    - Start watcher and defer cleanup
    - Pass logger to App for future use

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"ed-expedition/journal"
+	"ed-expedition/lib/fs"
 	"embed"
+	"flag"
+	"fmt"
+	"os"
 
 	"github.com/wailsapp/wails/v2"
+	wailsLogger "github.com/wailsapp/wails/v2/pkg/logger"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
@@ -12,11 +18,27 @@ import (
 var assets embed.FS
 
 func main() {
-	// Create an instance of the app structure
-	app := NewApp()
+	logger := wailsLogger.NewDefaultLogger()
 
-	// Create application with options
-	err := wails.Run(&options.App{
+	journalDir := flag.String("j", "", "The path to the journal files")
+	flag.Parse()
+	if len(*journalDir) == 0 || !fs.IsDir(*journalDir) {
+		logger.Error("Missing or invalid `-j` flag. You must provide a valid directory.")
+		os.Exit(1)
+	}
+
+	journalWatcher, err := journal.NewWatcher(*journalDir)
+	if err != nil {
+		logger.Error(fmt.Sprintf("Failed to watch journal directory: %e", err))
+		os.Exit(1)
+	}
+	defer journalWatcher.Close()
+
+	journalWatcher.Start()
+
+	app := NewApp(logger)
+
+	err = wails.Run(&options.App{
 		Title:  "ed-expedition",
 		Width:  1024,
 		Height: 768,
@@ -25,7 +47,7 @@ func main() {
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
-		Bind: []interface{}{
+		Bind: []any{
 			app,
 		},
 	})
