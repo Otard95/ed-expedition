1e8dc0902103b61d4c2d0d162f74038cd2022a34
Stian M <otard.code@proton.me>
2025-12-13 12:08:17 +0100

    feat(plotters): Add spansh data module with go:embed
    Embed minimal spansh reference data (FSD modules and Guardian FSD Boosters)
    for computing plotter parameters. Data pruned to only required fields.

---
+++ plotters/spansh_data.go
@@ -0,0 +1,41 @@
+package plotters
+
+import (
+	_ "embed"
+	"encoding/json"
+)
+
+//go:embed spansh.data.json
+var spanshDataJSON []byte
+
+var SpanshData *SpanshDataStruct
+
+type SpanshDataStruct struct {
+	Modules struct {
+		Standard struct {
+			FSD []SpanshFSDModule `json:"fsd"`
+		} `json:"standard"`
+		Internal struct {
+			GFSB []SpanshGFSBModule `json:"gfsb"`
+		} `json:"internal"`
+	} `json:"Modules"`
+}
+
+type SpanshFSDModule struct {
+	Symbol    string  `json:"symbol"`
+	OptMass   float64 `json:"optmass"`
+	MaxFuel   float64 `json:"maxfuel"`
+	FuelMul   float64 `json:"fuelmul"`
+	FuelPower float64 `json:"fuelpower"`
+}
+
+type SpanshGFSBModule struct {
+	Symbol    string  `json:"symbol"`
+	JumpBoost float64 `json:"jumpboost"`
+}
+
+func init() {
+	if err := json.Unmarshal(spanshDataJSON, &SpanshData); err != nil {
+		panic(err)
+	}
+}
