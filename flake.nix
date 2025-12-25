ec4afdb2442d8bb386ec024b77db3aec762c17f7
Otard95 <7838337+Otard95@users.noreply.github.com>
2025-12-25 13:43:04 +0000

    chore: update nix flake to v0.3.0 [skip ci]

--- flake.nix
+++ flake.nix
@@ -44,13 +44,13 @@
 
         packages.default = pkgs.stdenv.mkDerivation rec {
           pname = "ed-expedition";
-          version = "0.2.1";
+          version = "0.3.0";
 
           src = pkgs.fetchurl {
             url = "https://github.com/Otard95/ed-expedition/releases/download/v${version}/ed-expedition-linux-amd64-webkit2_41.tar.gz";
             # TODO: Update hash after first release (v0.0.1) is published
             # Run: nix-prefetch-url <url> to get the real hash
-            hash = "sha256-A8aVVrzuvdI1ZNVkPdXEXPBlPFkjHb2Ku+/38d2ecoo=";
+            hash = "sha256-7dk62H1JkdlYRLJkqjE7eyCMot/aZ9/aKp999gdoN2w=";
           };
 
           nativeBuildInputs = [ pkgs.makeWrapper ];
