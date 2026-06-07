1afc26542ba24d5da06884e8886dc37622ae0ce7
Stian M <otard.code@proton.me>
2026-06-07 14:02:20 +0200

    feat: Set application window icon on Linux
    Embed build/appicon.png and pass it to Linux window options.
    Windows and macOS use platform-specific icon mechanisms via
    wails build.

--- main.go
+++ main.go
@@ -10,10 +10,14 @@ import (
 	wailsLogger "github.com/wailsapp/wails/v2/pkg/logger"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/linux"
 )
 
 //go:embed all:frontend/dist
 var assets embed.FS
+
+//go:embed build/appicon.png
+var icon []byte
 
 func main() {
 	logger := wailsLogger.NewDefaultLogger()
@@ -37,6 +41,9 @@ func main() {
 			Assets: assets,
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		Linux: &linux.Options{
+			Icon: icon,
+		},
 		OnStartup:        app.startup,
 		OnShutdown:       app.shutdown,
 		Bind: []any{
